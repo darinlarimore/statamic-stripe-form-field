9491b45c5d6926e578a86906a8771b772c27ac2e
darinlarimore <darinlarimore@gmail.com>
2025-02-06 14:55:16 -0500

    spaces

--- config/statamic-stripe-form-field.php
+++ config/statamic-stripe-form-field.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-		'key' => env('STRIPE_PUBLISHABLE_KEY'),
-		'secret' => env('STRIPE_SECRET_KEY'),
-		'webhook_secret' => env('STRIPE_WEBHOOK_SIGNING_SECRET'),
+    'key' => env('STRIPE_PUBLISHABLE_KEY'),
+    'secret' => env('STRIPE_SECRET_KEY'),
+    'webhook_secret' => env('STRIPE_WEBHOOK_SIGNING_SECRET'),
 ];
